5be2ce6fd6e8ef9a86fe665482f6075a5bff19d2
Продавец03 <apxujiy3@yandex.ru>
2022-07-08 18:18:40 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+from random import sample
+
+list_traits = [
+    'Проклятый снайпер. Критудачи и критнеудачи 3-6 и 15-18 в любом случае',
+    'Толстокожий +25% ВУ',
+    'Точный +1 точность',
+    'Косой -1 точность',
+    'Стреляет только очередями',
+    'Всегда стреляет по самому слабому',
+    '+3 скорость',
+    'уменьшенный 2 раза урон от псионики',
+    'есть механика псионики на выбор',
+    'тупой - делает всякую хуйню',
+    'перезаряжается после каждого выстрела',
+    'механика удачи 2 раза в бой',
+    '+25% ХП',
+    '+2 уворот',
+    '+1 уворот в ход',
+
+]
+final_list = []
+
+user_input = input()
+user_list = list(user_input.split())
+if user_list[0] == 'roll':
+    try:
+        with open('traits.txt', 'w', encoding='utf-8') as t:
+            num = int(user_list[1])
+            choisen_traits = sample(list_traits, k=int(num+2))
+            for elem in choisen_traits:
+                t.write(f'{elem}\n')
+    except:
+        print('Число должно быть целым')
